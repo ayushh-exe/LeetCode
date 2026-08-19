0572d49c5cb1d20d34c8f626e38f2aa8b18ae849
Ayush Juyal <ayushjuyal.aj.7@gmail.com>
2026-08-19 17:49:14 +0530

    Time: 20 ms (36.65%), Space: 27.1 MB (22.84%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,50 +1,22 @@
 class Solution {
 public:
-    bool anagram(string s1,string s2)
-    {
-        int n=s1.length(), m=s2.length();
-        if(n==m)
-        {
-            sort(s1.begin(), s1.end());
-            sort(s2.begin(), s2.end());
-
-            if(s1==s2)
-            {
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-
-        return false;
-    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n=strs.size();
-        vector<vector<string>> ans;
-        vector<bool>visited(n,false);
-        for(int i=0;i<n;i++)
-        {
-            vector<string>group;
-            if(visited[i]){
-                continue;
-            }
+        unordered_map<string, vector<string>> mpp;
+        vector<vector<string>>ans;
 
-            visited[i]=true;
-            group.push_back(strs[i]);
-            for(int j=i+1; j<n;j++)
-            {
-                if(!visited[j] && anagram(strs[i],strs[j]))
-                {
-                    group.push_back(strs[j]);
-                    visited[j] = true;
-                }
-            }
-            ans.push_back(group);
-            
+        for(auto x:strs)
+        {
+            string temp=x;
+            sort(x.begin(), x.end());
+            mpp[x].push_back(temp);
         }
 
-        return ans;
+        for(auto a:mpp )
+        {
+            vector<string>temp=a.second;
+            ans.push_back(temp);
+        }
         
+        return ans;
     }
 };
